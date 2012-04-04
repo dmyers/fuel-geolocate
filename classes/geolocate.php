94db0352b5aa8dbc6e479dd28ca190f31e49620e
Derek Myers <arcticpro@gmail.com>
2012-04-03 21:28:15 -0700

    Fixing spacing and coding standard

--- classes/geolocate.php
+++ classes/geolocate.php
@@ -2,8 +2,6 @@
 
 namespace Geolocate;
 
-// ------------------------------------------------------------------------
-
 /**
  * Geolocate
  *
@@ -12,23 +10,23 @@ namespace Geolocate;
  * @author		Bryce Johnston
  */
 
-require_once(PKGPATH.'geolocate/classes/geoipcity.php');
+require_once PKGPATH.'geolocate/classes/geoipcity.php';
  
-class Geolocate {
-    
-    private static $_gi;
-    
-    /**
-     * get
-     * Uses GeoLiteCity.dat and geoip helpers to get Geo location information for the IP address passed
-     *
-     * @access  public
-     * @param   string    $ip_address     IP Address
-     * @param   boolean   $conn           TRUE or FALSE - whether a connection to GeoLiteCity is already open or not
-     * @return  array
-     */
-    public static function get($ip_address, $conn = FALSE)
-    {
+class Geolocate
+{
+	private static $_gi;
+	
+	/**
+	 * get
+	 * Uses GeoLiteCity.dat and geoip helpers to get Geo location information for the IP address passed
+	 *
+	 * @access  public
+	 * @param   string    $ip_address     IP Address
+	 * @param   boolean   $conn           TRUE or FALSE - whether a connection to GeoLiteCity is already open or not
+	 * @return  array
+	 */
+	public static function get($ip_address, $conn = FALSE)
+	{
 		($conn == FALSE ? static::_geoip_open() : 0);
 		$geoip = array();
 		$record = geoip_record_by_addr(static::$_gi, $ip_address);
@@ -42,32 +40,32 @@ class Geolocate {
 		}
 		($conn == FALSE ? static::_geoip_close() : 0);
 		return $geoip;
-    }
-    
-    /**
-     * _geoip_open
-     * Opens connection to GeoLiteCity.dat
-     *
-     * @access  private
-     * @return  void
-     */
-    private static function _geoip_open()
-    {
-        static::$_gi = geoip_open(PKGPATH.'geolocate/classes/GeoLiteCity.dat', GEOIP_STANDARD);
-    }
-    
-    /**
-     * _geoip_close
-     * Closes connection to GeoLiteCity.dat
-     *
-     * @access  private
-     * @return  void
-     */
-    private static function _geoip_close()
-    {
-        geoip_close(static::$_gi);
-    }
+	}
+	
+	/**
+	 * _geoip_open
+	 * Opens connection to GeoLiteCity.dat
+	 *
+	 * @access  private
+	 * @return  void
+	 */
+	private static function _geoip_open()
+	{
+		static::$_gi = geoip_open(PKGPATH.'geolocate/classes/GeoLiteCity.dat', GEOIP_STANDARD);
+	}
+	
+	/**
+	 * _geoip_close
+	 * Closes connection to GeoLiteCity.dat
+	 *
+	 * @access  private
+	 * @return  void
+	 */
+	private static function _geoip_close()
+	{
+		geoip_close(static::$_gi);
+	}
 
 }
 
-/* End of file session.php */
+/* End of file geolocate.php */
